d6ddb6846dc7ceeab2f7716eb813adcd82a5b531
Vitor Fonseca <vitorlofonseca@yahoo.com.br>
2019-10-16 20:58:00 -0300

    Get device endpoint

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 
 const { Device } = require("../models/Device/model");
+const models = { Device };
+
 const createDevice = require("../controllers/device/create");
 const authInDevice = require("../controllers/device/auth");
 const setVehicle = require("../controllers/device/setVehicle");
-
-const models = { Device };
+const getDevice = require("../controllers/device/getDevice");
 
 const routersInit = () => {
   const router = express();
   router.post("/device/create", createDevice(models));
   router.post("/device/auth", authInDevice(models));
   router.post("/device/setVehicle", setVehicle(models));
+  router.get("/device/:macAddress", getDevice(models));
   return router;
 };
 
---
+++ api/src/controllers/device/getDevice.js
@@ -0,0 +1,17 @@
+const getDevice = ({ Device }) => async (req, res, nex) => {
+  let macAddress = req.params.macAddress;
+
+  if (!macAddress) {
+    res.status(400).send("The MAC address must be passed through the URL");
+  }
+
+  let foundDevice = await Device.findOne({ macAddress: macAddress });
+
+  if (!foundDevice) {
+    res.status(404).send("Device not found");
+  }
+
+  res.status(200).send(foundDevice);
+};
+
+module.exports = getDevice;
