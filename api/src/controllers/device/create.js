b6dce35b07a5c478d74bf9980b468be12b6ff7a0
Vitor Fonseca <vitorlofonseca@yahoo.com.br>
2019-11-01 21:22:27 -0300

    Modifications on device creation

--- api/src/controllers/device/create.js
+++ api/src/controllers/device/create.js
@@ -1,27 +1,27 @@
-var sha256 = require("js-sha256").sha256;
-
 function validateNewDevice(device) {
   if (!device.password) {
-    res.status(400).send("A password field must be defined");
+    return "A password field must be defined";
   }
   if (!device.macAddress) {
-    res.status(400).send("A macAddress field must be defined");
+    return "A macAddress field must be defined";
   }
   if (device.inUse !== false && device.inUse !== true) {
-    res.status(400).send("A flag 'inUse' must be defined");
+    return "A flag 'inUse' must be defined";
   }
-}
 
-function encryptPassword(password) {
-  return sha256(password);
+  return false;
 }
 
 const create = ({ Device }) => (req, res, next) => {
   var device = new Device(req.body);
   device.metrics = { last: [], history: [] };
   try {
-    device.password = encryptPassword(req.body.password);
-    validateNewDevice(device);
+    let someDeviceError = validateNewDevice(device);
+
+    if (someDeviceError) {
+      res.status(400).send(someDeviceError);
+    }
+
     device.save();
     res.status(200).send("ok");
   } catch (e) {
